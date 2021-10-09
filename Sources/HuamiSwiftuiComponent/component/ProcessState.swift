4bda625aeddd83a9999f6bc041ba8ecd004fa4a3
Allan <allanholmes@163.com>
2021-10-09 11:17:10 +0800

    struct public

--- Sources/HuamiSwiftuiComponent/component/ProcessState.swift
+++ Sources/HuamiSwiftuiComponent/component/ProcessState.swift
@@ -8,7 +8,7 @@
 import SwiftUI
 
 @available(iOS 13.0, macOS 10.15, *)
-struct ProcessState: View {
+public struct ProcessState: View {
     var state:StateModel
     var foregroundColor:Color
     
