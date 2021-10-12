7a32ec4a62b6dfab2fd843fad854f4887f3d3660
Allan <allanholmes@163.com>
2021-10-12 14:40:06 +0800

    core-data add method

--- Sources/HuamiSwiftuiComponent/data/BaseCoreDataService.swift
+++ Sources/HuamiSwiftuiComponent/data/BaseCoreDataService.swift
@@ -32,7 +32,36 @@ open class BaseCoreDataService<ENTITY,MODEL> where ENTITY:NSManagedObject,MODEL:
         }
     }
     
-    public func listModel(request:NSFetchRequest<ENTITY>) -> [MODEL]{
+    open func addModel(model:MODEL){
+        let entity = model.toEntity()
+        do {
+            try viewContext.save()
+        }catch{
+            // Replace this implementation with code to handle the error appropriately.
+            // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
+            let nsError = error as NSError
+            fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
+        }
+    }
+    
+    open func deleteModels(request:NSFetchRequest<ENTITY>){
+        do {
+            let entities:[ENTITY] = try viewContext.fetch(request)
+            if entities.count > 0 {
+                for entity in entities {
+                    viewContext.delete(entity)
+                }
+            }
+            
+        } catch {
+            // Replace this implementation with code to handle the error appropriately.
+            // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
+            let nsError = error as NSError
+            fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
+        }
+    }
+    
+    open func listModels(request:NSFetchRequest<ENTITY>) -> [MODEL]{
         var result:[MODEL] = []
         do {
             let entities:[ENTITY] = try viewContext.fetch(request)
@@ -51,7 +80,7 @@ open class BaseCoreDataService<ENTITY,MODEL> where ENTITY:NSManagedObject,MODEL:
         return result
     }
     
-    public func getEntity(id:UUID) -> ENTITY?{
+    open func getEntity(id:UUID) -> ENTITY?{
         let request = NSFetchRequest<ENTITY>(entityName: entityName)
         request.predicate = NSPredicate(format: "id == %@", id as CVarArg)
         do {
